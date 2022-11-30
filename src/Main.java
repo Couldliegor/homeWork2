3fba4a6d5428596457e1f3c473fc6a2068edb9ec
egorcouldlie <couldliegor@gmail.com>
2022-11-30 21:25:48 +0300

    4 - 5

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+
+public class Main {
+    public static void main(String[] args) {
+                var dog = 8.0;
+                var cat = 8.6f;
+                var paper = 763789;
+                System.out.println(dog);
+                System.out.println(cat);
+                System.out.println(paper);
+                dog = dog + 4;
+                cat = cat + 4;
+                paper = paper + 4;
+                System.out.println(dog);
+                System.out.println(cat);
+                System.out.println(paper);
+                dog -= 3.5;
+                cat -= 1.6;
+                paper = paper - 7639;
+                    System.out.println(dog);
+                    System.out.println(cat);
+                    System.out.println(paper);
+            }
+        }
